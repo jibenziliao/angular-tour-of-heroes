888f6d53c5e4363646a5c2def5ade2ac0d53e870
jibenziliao <1501718947@qq.com>
2018-06-27 09:31:50 +0800

    fix:update heroes.component.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -25,7 +25,9 @@ export class HeroesComponent implements OnInit {
     if (!name) {
       return;
     }
-    this.heroService.addHero({name} as Hero).subscribe(hero => {this.heroes.push(hero)});
+    this.heroService.addHero({name} as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    });
   }
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
